1f318f2ddb166161999a32e62f559e75a12cbd22
FelipeAndrade448 <FelipeAndrade448@gmail.com>
2017-06-30 11:21:34 -0300

    Atualização WinForm

--- ExeciciosComplemetares/Menu.Designer.cs
+++ ExeciciosComplemetares/Menu.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
             this.txtValorAplicado = new System.Windows.Forms.TextBox();
             this.txtQuantMeses = new System.Windows.Forms.TextBox();
             this.txtJurosPoupanca = new System.Windows.Forms.TextBox();
@@ -43,13 +44,18 @@
             // 
             // txtValorAplicado
             // 
+            this.txtValorAplicado.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtValorAplicado.BackColor = System.Drawing.SystemColors.Window;
+            this.txtValorAplicado.ForeColor = System.Drawing.SystemColors.WindowText;
             this.txtValorAplicado.Location = new System.Drawing.Point(142, 62);
             this.txtValorAplicado.Name = "txtValorAplicado";
+            this.txtValorAplicado.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.txtValorAplicado.Size = new System.Drawing.Size(110, 20);
             this.txtValorAplicado.TabIndex = 2;
             // 
             // txtQuantMeses
             // 
+            this.txtQuantMeses.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtQuantMeses.Location = new System.Drawing.Point(142, 89);
             this.txtQuantMeses.Name = "txtQuantMeses";
             this.txtQuantMeses.Size = new System.Drawing.Size(110, 20);
@@ -57,6 +63,7 @@
             // 
             // txtJurosPoupanca
             // 
+            this.txtJurosPoupanca.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtJurosPoupanca.Location = new System.Drawing.Point(142, 116);
             this.txtJurosPoupanca.Name = "txtJurosPoupanca";
             this.txtJurosPoupanca.Size = new System.Drawing.Size(110, 20);
@@ -64,6 +71,7 @@
             // 
             // txtJurosRendaFixa
             // 
+            this.txtJurosRendaFixa.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.txtJurosRendaFixa.Location = new System.Drawing.Point(142, 143);
             this.txtJurosRendaFixa.Name = "txtJurosRendaFixa";
             this.txtJurosRendaFixa.Size = new System.Drawing.Size(110, 20);
@@ -71,6 +79,7 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(20, 65);
             this.label1.Name = "label1";
@@ -80,6 +89,7 @@
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(20, 93);
             this.label2.Name = "label2";
@@ -89,6 +99,7 @@
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(20, 120);
             this.label3.Name = "label3";
@@ -98,6 +109,7 @@
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(20, 147);
             this.label4.Name = "label4";
@@ -107,6 +119,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.Location = new System.Drawing.Point(47, 190);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
@@ -117,6 +130,7 @@
             // 
             // button2
             // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button2.Location = new System.Drawing.Point(154, 190);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
@@ -127,6 +141,7 @@
             // 
             // label5
             // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(12, 19);
@@ -140,6 +155,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(282, 249);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
@@ -152,7 +168,11 @@
             this.Controls.Add(this.txtJurosPoupanca);
             this.Controls.Add(this.txtQuantMeses);
             this.Controls.Add(this.txtValorAplicado);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Menu";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.Text = "Calculadora Financeira";
             this.ResumeLayout(false);
             this.PerformLayout();
--- ExercicioComplementar/Program.cs
+++ ExercicioComplementar/Program.cs
@@ -11,23 +11,29 @@ namespace ExercicioComplementar
         static void Main(string[] args)
         {
             Calculadora calc = new Calculadora();
+            
+            void EntradaDeDados()
+            {
+                Console.WriteLine("------------------------------------------------------");
+                Console.Write("Valor a ser aplicado: R$");
+                calc.valorAplicado = double.Parse(Console.ReadLine());
+                Console.Write("Quantidade de Meses a ser aplicado: ");
+                calc.quantMeses = int.Parse(Console.ReadLine());
+                Console.Write("Taxa de Juros Poupança: ");
+                calc.jurosMesPoupanca = double.Parse(Console.ReadLine());
+                Console.Write("Taxa de Juros Renda Fixa: ");
+                calc.jurosMesRendaFixa = double.Parse(Console.ReadLine());
+                Console.WriteLine("");
+                Console.WriteLine("------------------------------------------------------");
+            }
 
-            Console.Write("Valor a ser aplicado: R$");
-            calc.valorAplicado = double.Parse(Console.ReadLine());
-            Console.Write("Quantidade de Meses a ser aplicado: ");
-            calc.quantMeses = int.Parse(Console.ReadLine());
-            Console.Write("Taxa de Juros Poupança: ");
-            calc.jurosMesPoupanca = double.Parse(Console.ReadLine());
-            Console.Write("Taxa de Juros Renda Fixa: ");
-            calc.jurosMesRendaFixa = double.Parse(Console.ReadLine());
-            Console.WriteLine("");
-            Console.WriteLine("------------------------------------------------------");
-
+            EntradaDeDados();
             calc.JurosCompostoPoupanca();
             calc.JurosCompostoRendaFixa();
             calc.ImpostoDeRenda();
             calc.Resposta();
             Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------------");
             Console.ReadLine();
         }
     }
