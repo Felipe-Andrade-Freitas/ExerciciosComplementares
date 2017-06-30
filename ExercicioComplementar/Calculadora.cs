29c3fa5b143c8f1cd239a9a6a3d31d07652df4c8
FelipeAndrade448 <FelipeAndrade448@gmail.com>
2017-06-30 09:55:32 -0300

    Console feito em Orientação a Objetos

---
+++ ExercicioComplementar/Calculadora.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioComplementar
+{
+    class Calculadora
+    {
+        #region Variaveis
+
+        public double valorAplicado { get; set; }
+        public int quantMeses { get; set; }
+        public double jurosMesPoupanca { get; set; }
+        public double jurosMesRendaFixa { get; set; }
+        public double rendMensalPoupanca { get; set; }
+        public double rendTotalPoupanca { get; set; }
+        public double rendMensalRendaFixa { get; set; }
+        public double rendMensalRendaFixaImposto { get; set; }
+        public double rendTotalRendaFixa { get; set; }
+        public double impostoDeRenda { get; set; }
+        public string op1 = "Poupança", op2 = "Renda Fixa";
+
+        #endregion
+
+        #region Metodos
+
+        public void JurosCompostoPoupanca()
+        {
+            for (int i = 0; i < quantMeses; i++)
+            {
+                rendMensalPoupanca = (valorAplicado * jurosMesPoupanca * quantMeses) / 100;
+            }
+
+            rendTotalPoupanca = valorAplicado + rendMensalPoupanca;
+        }
+
+        public void JurosCompostoRendaFixa()
+        {
+            for (int i = 0; i < quantMeses; i++)
+            {
+                rendMensalRendaFixa = (valorAplicado * quantMeses * jurosMesRendaFixa) / 100;
+                rendTotalRendaFixa = valorAplicado + rendMensalRendaFixa;
+            }
+
+        }
+
+        public void ImpostoDeRenda()
+        {
+            if(quantMeses <= 12)
+            {
+                impostoDeRenda = rendMensalRendaFixa * 0.25;
+                rendMensalRendaFixaImposto = valorAplicado - impostoDeRenda + rendMensalRendaFixa;
+            }
+            else if(quantMeses > 12 && quantMeses <= 24 )
+            {
+                impostoDeRenda = rendMensalRendaFixa * 0.15;
+                rendMensalRendaFixaImposto = valorAplicado - impostoDeRenda + rendMensalRendaFixa;
+            }
+            else if(quantMeses > 24 && quantMeses <= 36)
+            {
+                impostoDeRenda = rendMensalRendaFixa * 0.5;
+                rendMensalRendaFixaImposto = valorAplicado - impostoDeRenda + rendMensalRendaFixa;
+            }
+            else if(quantMeses > 36)
+            {
+                impostoDeRenda = rendMensalRendaFixa * 0.1;
+                rendMensalRendaFixaImposto = valorAplicado - impostoDeRenda + rendMensalRendaFixa;
+            }
+        }
+
+        public void OpcaoDeInvestimento()
+        {
+            if(rendTotalPoupanca > rendMensalRendaFixaImposto)
+            {
+                Console.WriteLine("A melhor opção para investimento é: {0}", op1);
+            }
+            else
+            {
+                Console.WriteLine("A melhor opção para investimento é: {0}", op2);
+            }
+        }
+        
+        public void Resposta()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("O valor do rendimento da Poupança será de: R${0}", rendMensalPoupanca);
+            Console.WriteLine("O valor do  retorno da Poupanca será de: R${0}", rendTotalPoupanca);
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("O valor do rendimento da Renda Fixa será de: R${0}\nO seu imposto de renda é: R${1}", rendMensalRendaFixa, impostoDeRenda);
+            Console.WriteLine("O valor do retorno da Renda Fixa será de: R${0}", rendMensalRendaFixaImposto);
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------------");
+            Console.WriteLine("");
+            OpcaoDeInvestimento();
+        }
+
+        #endregion
+
+        
+
+    }
+}
--- ExercicioComplementar/Program.cs
+++ ExercicioComplementar/Program.cs
@@ -10,159 +10,24 @@ namespace ExercicioComplementar
     {
         static void Main(string[] args)
         {
-            #region Variaveis
-            //Variáveis
-            double valorInvestido, Poupanca, rendaFixa;
-            int meses;
+            Calculadora calc = new Calculadora();
 
-            //Variáveis relacionado a Poupança
-            double TotalPoupanca, valorFinalPoupanca;
-            string op1 = "Poupança";
+            Console.Write("Valor a ser aplicado: R$");
+            calc.valorAplicado = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade de Meses a ser aplicado: ");
+            calc.quantMeses = int.Parse(Console.ReadLine());
+            Console.Write("Taxa de Juros Poupança: ");
+            calc.jurosMesPoupanca = double.Parse(Console.ReadLine());
+            Console.Write("Taxa de Juros Renda Fixa: ");
+            calc.jurosMesRendaFixa = double.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------------");
 
-            //Variáveis relacionado a Renda Fixa
-            double TotalRendaFixa;
-            double valorFinalRendaFixa;
-            double valorFinalRendaFixaImposto;
-            double impostoDeRenda;
-            double valorRendaFixaImpostoRenda;
-            string op2 = "Renda Fixa";
-            #endregion
-
-            //Iniciando a Calculadora Financeira
-            Console.WriteLine("Bem Vindo á Calculadora Financeira");
-            Console.WriteLine("Responda as perguntas abaixo: ");
+            calc.JurosCompostoPoupanca();
+            calc.JurosCompostoRendaFixa();
+            calc.ImpostoDeRenda();
+            calc.Resposta();
             Console.WriteLine("");
-
-            //Entrada de Dados
-            Console.Write("Qual o valor a ser aplicado? R$");
-            valorInvestido = double.Parse(Console.ReadLine());
-            Console.Write("Qual a porcentagem dos rendimentos mensais para poupaca? ");
-            Poupanca = double.Parse(Console.ReadLine());
-            Console.Write("Qual a porcentagem dos rendimentos mensais para renda fixa? ");
-            rendaFixa = double.Parse(Console.ReadLine());
-            Console.Write("Qual a quantidade de meses que o dinheiro ficará aplicado? ");
-            meses = int.Parse(Console.ReadLine());
-            Console.WriteLine("");
-
-            //Saida de Dados
-
-            //Poupança
-            Console.WriteLine("Poupança: ");
-            Console.WriteLine("");
-            TotalPoupanca = Poupanca * valorInvestido * meses / 100;
-            valorFinalPoupanca = TotalPoupanca + valorInvestido;
-            Console.WriteLine("O valor que renderá para poupanca será de R$" + TotalPoupanca + ".");
-            Console.WriteLine("O valor do seu investimento mais o seu valor aplicado será de R$" + valorFinalPoupanca + ".");
-            Console.WriteLine("");
-            Console.WriteLine("");
-            
-            //Renda Fixa
-            Console.WriteLine("Renda Fixa: ");
-            Console.WriteLine("");
-            TotalRendaFixa = rendaFixa * valorInvestido * meses / 100;
-            valorFinalRendaFixa = TotalRendaFixa + valorInvestido;
-            Console.WriteLine("O valor que renderá para renda fixa será de R$" + valorFinalRendaFixa);
-            Console.WriteLine("");
-            Console.WriteLine("");
-
-            //Imposto de Renda
-            if(meses <= 12)
-            {
-                impostoDeRenda = TotalRendaFixa * 25 / 100;
-                valorRendaFixaImpostoRenda = TotalRendaFixa - impostoDeRenda;
-                valorFinalRendaFixaImposto = TotalRendaFixa + valorInvestido - impostoDeRenda;
-                Console.WriteLine("Imposto de Renda: ");
-                Console.WriteLine("");
-                Console.WriteLine("Você pagara R$" + impostoDeRenda + " de imposto de renda");
-                Console.WriteLine("O valor que renderá para renda fixa menos o imposto de renda será de R$" + valorRendaFixaImpostoRenda + ".");
-                Console.WriteLine("O valor do seu investimento mais o seu valor aplicado menos o imposto será de R$" + valorFinalRendaFixaImposto + ".");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("Opção de investimento: ");
-                Console.WriteLine("");
-                if (valorFinalPoupanca > valorFinalRendaFixaImposto)
-                {
-                    Console.WriteLine("A melhor opção de invesimentos é " + op1 + ".");
-                }
-                else
-                {
-                    Console.WriteLine("A melhor opção de investimento é " + op2 + ".");
-                }
-                Console.WriteLine("");
-            }
-            else if(meses > 13 && meses > 24)
-            {
-                impostoDeRenda = TotalRendaFixa * 15 / 100;
-                valorRendaFixaImpostoRenda = TotalRendaFixa - impostoDeRenda;
-                valorFinalRendaFixaImposto = TotalRendaFixa + valorInvestido - impostoDeRenda;
-                Console.WriteLine("Imposto de Renda: ");
-                Console.WriteLine("");
-                Console.WriteLine("Você pagara R$" + impostoDeRenda + " de imposto de renda");
-                Console.WriteLine("O valor que renderá para renda fixa menos o imposto de renda será de R$" + valorRendaFixaImpostoRenda + ".");
-                Console.WriteLine("O valor do seu investimento mais o seu valor aplicado menos o imposto será de R$" + valorFinalRendaFixaImposto + ".");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("Opção de investimento: ");
-                Console.WriteLine("");
-                if (valorFinalPoupanca > valorFinalRendaFixaImposto)
-                {
-                    Console.WriteLine("A melhor opção de invesimentos é " + op1 + ".");
-                }
-                else
-                {
-                    Console.WriteLine("A melhor opção de investimento é " + op2 + ".");
-                }
-                Console.WriteLine("");
-            }
-            else if(meses > 25 && meses < 36)
-            {
-                impostoDeRenda = TotalRendaFixa * 5 / 100;
-                valorRendaFixaImpostoRenda = TotalRendaFixa - impostoDeRenda;
-                valorFinalRendaFixaImposto = TotalRendaFixa + valorInvestido - impostoDeRenda;
-                Console.WriteLine("Imposto de Renda: ");
-                Console.WriteLine("");
-                Console.WriteLine("Você pagara R$" + impostoDeRenda + " de imposto de renda");
-                Console.WriteLine("O valor que renderá para renda fixa menos o imposto de renda será de R$" + valorRendaFixaImpostoRenda + ".");
-                Console.WriteLine("O valor do seu investimento mais o seu valor aplicado menos o imposto será de R$" + valorFinalRendaFixaImposto + ".");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("Opção de investimento: ");
-                Console.WriteLine("");
-                if (valorFinalPoupanca > valorFinalRendaFixaImposto)
-                {
-                    Console.WriteLine("A melhor opção de invesimentos é " + op1 + ".");
-                }
-                else
-                {
-                    Console.WriteLine("A melhor opção de investimento é " + op2 + ".");
-                }
-                Console.WriteLine("");
-            }
-            else if(meses > 36)
-            {
-                impostoDeRenda = TotalRendaFixa * 1 / 100;
-                valorRendaFixaImpostoRenda = TotalRendaFixa - impostoDeRenda;
-                valorFinalRendaFixaImposto = TotalRendaFixa + valorInvestido - impostoDeRenda;
-                Console.WriteLine("Imposto de Renda: ");
-                Console.WriteLine("");
-                Console.WriteLine("Você pagara R$" + impostoDeRenda + " de imposto de renda");
-                Console.WriteLine("O valor que renderá para renda fixa menos o imposto de renda será de R$" + valorRendaFixaImpostoRenda + ".");
-                Console.WriteLine("O valor do seu investimento mais o seu valor aplicado menos o imposto será de R$" + valorFinalRendaFixaImposto + ".");
-                Console.WriteLine("");
-                Console.WriteLine("");
-                Console.WriteLine("Opção de investimento: .");
-                Console.WriteLine("");
-                if (valorFinalPoupanca > valorFinalRendaFixaImposto)
-                {
-                    Console.WriteLine("A melhor opção de invesimentos é " + op1 + ".");
-                }
-                else
-                {
-                    Console.WriteLine("A melhor opção de investimento é " + op2 + ".");
-                }
-                Console.WriteLine("");
-            }
-
             Console.ReadLine();
         }
     }
